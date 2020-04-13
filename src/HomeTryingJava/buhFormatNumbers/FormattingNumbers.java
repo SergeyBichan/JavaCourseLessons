22ffd7aa49935d2fcf2d792001fb88ae01929789
Sergey Bichan <siarhei.bichan@gmail.com>
2020-04-13 14:33:01 +0300

    вывод числа в бух.формате

---
+++ src/HomeTryingJava/buhFormatNumbers/FormattingNumbers.java
@@ -0,0 +1,8 @@
+package HomeTryingJava.buhFormatNumbers;
+
+public class FormattingNumbers {
+    public static void main(String[] args) {
+        int num = 20023143;
+        System.out.printf("%,d", num);
+    }
+}
