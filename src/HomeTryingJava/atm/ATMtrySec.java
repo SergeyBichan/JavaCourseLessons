9827e815e2eba129166c69df456cd9eec7df2341
Sergey BIchan <ugidancer@Gmail.com>
2020-04-30 00:41:22 +0300

    всё на костылях

--- src/HomeTryingJava/atm/ATMtrySec.java
+++ src/HomeTryingJava/atm/ATMtrySec.java
@@ -7,8 +7,9 @@ public class ATMtrySec {
     int cash20;
     int cash50;
     int cash100;
-    int cashAtStart;
-    int balance;
+    int cashAtStart=1000;
+    public int balance =getCash20() + getCash50() + getCash100() + getCashAtStart();
+    public int wannaCash;
 
 
     public ATMtrySec() {
@@ -23,7 +24,7 @@ public class ATMtrySec {
     }
 
     public int getCash20() {
-        return cash20*20;
+        return cash20 * 20;
     }
 
     public void setCash20(int cash20) {
@@ -31,7 +32,7 @@ public class ATMtrySec {
     }
 
     public int getCash50() {
-        return cash50*50;
+        return cash50 * 50;
     }
 
     public void setCash50(int cash50) {
@@ -39,7 +40,7 @@ public class ATMtrySec {
     }
 
     public int getCash100() {
-        return cash100*100;
+        return cash100 * 100;
     }
 
     public void setCash100(int cash100) {
@@ -54,12 +55,18 @@ public class ATMtrySec {
         this.cashAtStart = cashAtStart;
     }
 
-    public int addCash(int a, int b, int c, int d){
-        return a+b+c+d;
+    public int addCash(int a, int b, int c, int d) {
+        return a + b + c + d;
     }
 
+    public int getCash(int a, int b) {
+        return balance = a - b;
+    }
+
+
     public void atmStartScreen() {
-        System.out.println("Hello user! How much money u want to add?");
+        System.out.println("Hello user! We have " + getCashAtStart());
+
     }
 
     public void printMenu() {
@@ -68,13 +75,12 @@ public class ATMtrySec {
         System.out.println("3. I want to see balance: ");
     }
 
-    public void atmWork(){
+    public void atmWork() {
         Scanner in = new Scanner(System.in);
-        setCashAtStart(in.nextInt());
-        System.out.println("U added " + getCashAtStart() + "$");
+        //setCashAtStart(in.nextInt());
         System.out.println("What do u want to do?");
         printMenu();
-        switch (in.nextInt()){
+        switch (in.nextInt()) {
             case 1:
                 System.out.println("Deposit 20$");
                 setCash20(in.nextInt());
@@ -82,12 +88,32 @@ public class ATMtrySec {
                 setCash50(in.nextInt());
                 System.out.println("Deposit 100$");
                 setCash100(in.nextInt());
+                balance = addCash(getCash20(), getCash50(), getCash100(), getCashAtStart());
                 System.out.println("Ur balance is " +
                         addCash(getCash20(),
                                 getCash50(),
                                 getCash100(),
                                 getCashAtStart()));
+                atmWork();
+                break;
+            case 2:
+                System.out.println("How much money u want to take?");
+                wannaCash = in.nextInt();
+                if (getBalance() > wannaCash) {
+                    System.out.println("After this operation we have "
+                            + getCash(getBalance(), wannaCash));
+                    atmWork();
+                } else {
+                    System.out.println("we dont have money");
+                    atmWork();
+                }
+
+            case 3:
+                System.out.println("Balance is " + balance);
+                atmWork();
+
         }
+
     }
 
 }
