18736929aa67a0495394e69ea3edaf5979619f74
ugidancer@Gmail.com <ugidancer@gmail.com>
2020-12-14 20:44:02 +0300

    lesson7_memory

---
+++ src/by/academy/lesson7/Main.java
@@ -0,0 +1,8 @@
+package by.academy.lesson7;
+
+public class Main {
+    public static void main(String[] args) {
+        Parent parent = new Kid();
+        parent.create(5);
+    }
+}
---
+++ src/by/academy/lesson7/Parent.java
@@ -0,0 +1,48 @@
+package by.academy.lesson7;
+
+public class Parent {
+    public void create(int i){
+        if (i > 5){
+            go();
+        }
+        else {
+            stop();
+        }
+
+    }
+
+    private void go(){
+        System.out.println("GO");
+        System.out.println("GO");
+        System.out.println("GO");
+        System.out.println("GO");
+        System.out.println("GO");
+        System.out.println("GO");
+        System.out.println("GO");
+        System.out.println("GO");
+        System.out.println("GO");
+        System.out.println("GO");
+        System.out.println("GO");
+        System.out.println("GO");
+        System.out.println("GO");
+        System.out.println("GO");
+    }
+
+    private void stop(){
+        System.out.println("STOP");
+        System.out.println("STOP");
+        System.out.println("STOP");
+        System.out.println("STOP");
+        System.out.println("STOP");
+        System.out.println("STOP");
+        System.out.println("STOP");
+        System.out.println("STOP");
+        System.out.println("STOP");
+        System.out.println("STOP");
+        System.out.println("STOP");
+        System.out.println("STOP");
+        System.out.println("STOP");
+        System.out.println("STOP");
+        System.out.println("STOP");
+    }
+}
